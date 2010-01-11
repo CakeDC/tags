3378efd6f8dd4fb212fff67d3dfe2b46529ad921
renan.saddam <renan.saddam@gmail.com>
2010-01-11 16:45:27 -0200

    Fixing typo.

--- config/migrations/map.php
+++ config/migrations/map.php
@@ -1,5 +1,5 @@
 <?php
 $map = array(
-	1 => array('001_initialize_ratings_schema' => 'M49ac311a54844a9d87o822502jedc423')
+	1 => array('001_initialize_tags_schema' => 'M49ac311a54844a9d87o822502jedc423')
 );
 ?>
