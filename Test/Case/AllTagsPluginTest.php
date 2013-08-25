8d3edc3e845cdde1ebbe555bbf3562cb392c4b86
Florian Krämer <florian.kraemer@cakedc.com>
2013-08-26 00:59:44 +0200

    Reverting AllTagsPluginTest.php

--- Test/Case/AllTagsPluginTest.php
+++ Test/Case/AllTagsPluginTest.php
@@ -10,7 +10,15 @@ class AllTagsPluginTest extends PHPUnit_Framework_TestSuite {
 		$suite = new PHPUnit_Framework_TestSuite('All Tags Plugin Tests');
 
 		$basePath = CakePlugin::path('Tags') . DS . 'Test' . DS . 'Case' . DS;
-		$suite->addTestDirectoryRecursive($basePath);
+		// controllers
+		$suite->addTestFile($basePath . 'Controller' . DS . 'TagsControllerTest.php');
+		// behaviors
+		$suite->addTestFile($basePath . 'Model' . DS . 'Behavior' . DS . 'TaggableTest.php');
+		// models
+		$suite->addTestFile($basePath . 'Model' . DS . 'TagTest.php');
+		$suite->addTestFile($basePath . 'Model' . DS . 'TaggedTest.php');
+		// helpers
+		$suite->addTestFile($basePath . 'View' . DS . 'Helper' . DS . 'TagCloudTest.php');
 		return $suite;
 	}
 
