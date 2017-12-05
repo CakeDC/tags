1f5a2bd5900f2f6142107564e7df3186a24e0eff
Roger Campanera <git@roger.pro>
2017-12-05 12:52:56 +0000

    Restored Copyright & updated year to 2017

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -1,4 +1,14 @@
 <?php 
+/**
+ * Copyright 2009-2017, Cake Development Corporation (http://cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2009-2017, Cake Development Corporation (http://cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
 class TagsSchema extends CakeSchema {
 
 	public function before($event = array()) {
